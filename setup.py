112b15ec68c25b6dba8571038126ea10a1c2ea53
Jae Yong Lee <leejaeyong7@gmail.com>
2020-03-10 11:27:27 -0500

    modifying manifest

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/leejaeyong7/rmvsnet-pytorch",
     packages=setuptools.find_packages(),
+    package_data={
+      'weights': ['rmvsnet/RMVSNET-pretrained.pth']
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
